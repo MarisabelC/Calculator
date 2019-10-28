bbc396886165cd16b0fe80cd922f54381b104bdb
Marisabel Chang <marisabelchang@gmail.com>
2019-10-28 12:22:08 -0700

    add getTotal method To Evaluator class

--- src/main/java/calculator/app/Evaluator.java
+++ src/main/java/calculator/app/Evaluator.java
@@ -4,23 +4,50 @@ import java.security.InvalidParameterException;
 import java.util.NoSuchElementException;
 
 public class Evaluator {
-	
+
 	private Tokenizer tokenizer;
-	
 
 	public Evaluator() {
-		tokenizer= new Tokenizer();
+		tokenizer = new Tokenizer();
 	}
-	
+
 	public void setExpression(String expression) {
 		try {
 			tokenizer.set(expression);
-		}catch(InvalidParameterException e) {
+		} catch (InvalidParameterException e) {
 			System.out.println(e);
 		}
 	}
-	
-	
 
+	public double getTotal() {
+		double LHS = Double.parseDouble(getNextToken(tokenizer).getValue());
+		String operator = getNextToken(tokenizer).getValue();
+		double RHS = Double.parseDouble(getNextToken(tokenizer).getValue());
+		return assignOperator(LHS, RHS, operator);
+	}
+
+	private Token getNextToken(Tokenizer tokenizer) throws NoSuchElementException {
+		if (tokenizer.isMoreToken())
+			return tokenizer.getNextToken();
+		throw new NoSuchElementException("No more token");
+	}
+
+	private double assignOperator(double LHS, double RHS, String operator) {
+		Operator total = new Operator();
+		switch (operator) {
+		case "+":
+			return total.add(LHS, RHS);
+		case "-":
+			return total.subtract(LHS, RHS);
+		case "x":
+			return total.multiply(LHS, RHS);
+		case "/":
+			return total.divide(LHS, RHS);
+		case "%":
+			return total.mod(LHS, RHS);
+		default:
+		}
+		return 0;
+	}
 
 }
--- src/test/java/calculator/app/EvaluatorTest.java
+++ src/test/java/calculator/app/EvaluatorTest.java
@@ -20,4 +20,43 @@ class EvaluatorTest {
 		evaluator.setExpression("5+2-");
 	}
 
+	@Test
+	void getTotalDivisionTest() {
+		evaluator = new Evaluator();
+	    evaluator.setExpression("5/2");
+		double result = evaluator.getTotal();
+		assertEquals(2.5, result,0.001);
+	}
+	
+	@Test
+	void getTotalMultiplyTest() {
+		evaluator = new Evaluator();
+	    evaluator.setExpression("2.5 x 6");
+		double result = evaluator.getTotal();
+		assertEquals(15, result);
+	}
+	
+	@Test
+	void getTotalModTest() {
+		evaluator = new Evaluator();
+	    evaluator.setExpression("3 % 5");
+		double result = evaluator.getTotal();
+		assertEquals(3, result);
+	}
+	
+	@Test
+	void getTotalSubtractTest() {
+		evaluator = new Evaluator();
+	    evaluator.setExpression("30 - 55");
+		double result = evaluator.getTotal();
+		assertEquals(-25, result);
+	}
+	
+	@Test
+	void getTotalAddTest() {
+		evaluator = new Evaluator();
+	    evaluator.setExpression("30 + 25");
+		double result = evaluator.getTotal();
+		assertEquals(55, result);
+	}
 }
