f7b9a8f0d9069d87c1c3b41eadc40974c7ab1f28
Marisabel Chang <marisabelchang@gmail.com>
2019-10-28 22:10:27 -0700

    add hasnext method for scanner

--- src/main/java/calculator/app/Calculator.java
+++ src/main/java/calculator/app/Calculator.java
@@ -9,21 +9,25 @@ public class Calculator {
 		
 		Scanner sc = new Scanner(System.in);
 		Evaluator evaluator = new Evaluator();
-		String expression;
+		String expression="n";
 		
 		do {
 			System.out.println("Please add the arithmetic expression");
-			expression = sc.nextLine(); 
-			try {
-				evaluator.setExpression(expression);
-				System.out.println(expression + " = "+ evaluator.getTotal());
-			}catch (ArithmeticException e) {
-				System.out.println(e);
-			}catch (InvalidParameterException e) {
-				System.out.println(e);
+			while(sc.hasNextLine()) {
+				expression = sc.nextLine(); 
+				try {
+					evaluator.setExpression(expression);
+					System.out.println(expression + " = "+ evaluator.getTotal());
+				}catch (ArithmeticException e) {
+					System.out.println(e);
+				}catch (InvalidParameterException e) {
+					System.out.println(e);
+				}
+				
+				System.out.println("Do you want to calculate anoter arithmetic expression? Y/N");
+				if (sc.hasNextLine())
+					expression=sc.nextLine();
 			}
-			System.out.println("Do you want to calculate anoter arithmetic expression? Y/N");
-			expression=sc.nextLine();
 		} while(expression.trim().equalsIgnoreCase("y")) ;
 		System.out.println("Thank you for using the calculator app");
 	}
